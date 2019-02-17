c9d74021357d6e4de39c63c2d83ecf31231fd13a
Jacob Charley Coffland <cofflaj@LabImage2016-17>
2019-02-16 18:30:13 -0800

    state machine kinda

--- Interactivephone/app/src/main/java/com/example/cofflag/interactivephone/AccelerationListener.java
+++ Interactivephone/app/src/main/java/com/example/cofflag/interactivephone/AccelerationListener.java
@@ -8,6 +8,8 @@ import android.media.MediaPlayer;
 import android.util.Log;
 import android.view.View;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by cofflag on 2/16/19.
  */
@@ -32,21 +34,60 @@ public class AccelerationListener implements SensorEventListener{
         float z = event.values[2];
         double f = Math.sqrt(x*x+y*y+z*z);
         Log.d("#jacob","X: "+x+" Y: "+y+" Z: "+z + " ||V||: " + f);
-        if(f < 1 && f > -1){
-            if(!screaming) {
-                mediaplayer.start();
-                screaming = true;
-                Log.d("SCREAM", "SCREAM");
+        boolean falling = (-1 < f && f < 1);
+        boolean playing = mediaplayer.isPlaying();
+        boolean atZero = mediaplayer.getCurrentPosition() == 0;
+        if(playing) {
+            if(falling) {
+                // good job!
+            }
+            else {
+                if(atZero) {
+                    mediaplayer.pause();
+                }
+                else {
+                    mediaplayer.pause();
+                    mediaplayer.seekTo(0);
+                }
             }
         }
-        if(f > 1 || f < -1){
-            mediaplayer.stop();
-            screaming = false;
-            try {
-                mediaplayer.prepare();
-            } catch (Exception e) {
-                //do nothing
+        else { // not playing
+            if(falling){
+                if(atZero){
+                    mediaplayer.start();
+                }
+                else {
+                    // Hopefully this never happens
+                    Log.d("log", "...shit");
+                }
+            } else {
+                if(atZero) {
+                    // good job!
+                } else {
+                    mediaplayer.seekTo(0);
+                }
+
             }
         }
+
     }
 }
+/*if(f < 1 && f > -1){
+        if(!screaming) {
+        mediaplayer.start();
+        screaming = true;
+        try {
+        TimeUnit.SECONDS.sleep(1);
+        } catch (Exception e) {
+
+        }
+        Log.d("SCREAM", "SCREAM");
+        }
+        }
+        else if (screaming) {
+        if(mediaplayer.isPlaying()){
+        mediaplayer.pause();
+        }
+        mediaplayer.seekTo(0);
+        screaming = false;
+        }*/
